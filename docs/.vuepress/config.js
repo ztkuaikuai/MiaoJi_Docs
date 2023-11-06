ea01d9a906d07898fe0f19039c044e893ec742cc
ztkuaikuai <2647731903@qq.com>
2023-11-06 16:09:37 +0800

    doc: 新增模板、用户文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,12 +5,21 @@ export default defineUserConfig({
   title: '妙记',
   description: '妙记使用手册',
   theme: defaultTheme({
-     // 默认主题配置
-     navbar: [
+    locales: {
+      '/' : {
+        lang: 'zh-CN',
+        title: '妙记',
+        description: '妙记使用手册',
+      }
+    },
+    // 默认主题配置
+    navbar: [
       {
         text: '首页',
         link: '/',
       },
     ],
+    logo: '/img/logo_circle.png',
+    repo: 'ztkuaikuai/MiaoJi_Docs'
   })
 })
